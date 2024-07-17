fc0b50206880e22a81a1b192523677228c72976b
adhsandb05 <adhikarisandeeb@gmail.com>
2024-07-17 11:50:47 -0400

    "import for openai changed"

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import OpenAI from 'openai';
-import * as dotenv from 'dotenv';
+import express, { response } from 'express'
+import { OpenAI} from 'openai'
+import * as dotenv from 'dotenv'
 
 
 dotenv.config();
@@ -8,32 +8,33 @@ dotenv.config();
 const router = express.Router();
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,  // Ensure the key is correctly spelled
-});
+    //organization:"org-LYaBegBeM9wvD3EWBguM9oK1",
+    apiKey: process.env.OPENAI_APT_KEY,
+})
 
 
-router.route('/').get((req, res) => {
-    res.status(200).json({ message: "Hello from DALL.E ROUTES" });
-});
 
-router.route('/generate').post(async (req, res) => {  
+router.route('/').get((req,res)=>{
+    res.status(200).json({message: "Hello from DALL.E ROUTES"})
+})
+router .route('/').post(async (req, res)=>{
     try {
-        const { prompt } = req.body;
+        const {prompt} = req.body;
 
         const response = await openai.images.generate({
+            model:"dall-e-3",
             prompt,
             n: 1,
             size: '1024x1024',
-            response_format: 'b64_json'
+            response_format: 'b64_json',
         });
 
-        const image = response.data.data[0].b64_json;
+        const image = response.data[0].b64_json;
 
-        res.status(200).json({ photo: image });
-    } catch (error) {
+        res.status(200).json({photo: image});
+    }catch(error){
         console.error(error);
-        res.status(500).json({ message: "Something went wrong" });
+        res.status(500).json({message: "Something went wrong"})
     }
-});
-
+})
 export default router;
