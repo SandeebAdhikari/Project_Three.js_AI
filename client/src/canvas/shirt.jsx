abab27841e38bcb2b0e44d716510a3d204e3fe99
adhsandb05 <adhikarisandeeb@gmail.com>
2024-08-03 18:43:19 -0400

    add more glb files

--- client/src/canvas/shirt.jsx
+++ client/src/canvas/shirt.jsx
@@ -12,34 +12,31 @@ const shirt = () => {
 
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
-  
+
   const meshRef = useRef();
 
   useFrame((state, delta) => {
     if (materials.Image) {
       easing.dampC(materials.Image.color, snap.color, 0.25, delta);
     }
-    
   });
 
   const stateString = JSON.stringify(snap);
   return (
-    <group
-      key={stateString}
-    >
+    <group key={stateString}>
       <mesh
-        castShadow 
+        castShadow
         geometry={nodes.Cylinder.geometry}
         material={materials.Image}
         material-roughness={1}
         dispose={null}
-        scale={[5,5,5]}
+        scale={[5, 5, 5]}
       >
         {snap.isFullTexture && (
           <Decal
             position={[0, 0, 0.16]}
             rotation={[0, 0, 0]}
-            scale={0.30}
+            scale={0.3}
             map={fullTexture}
           />
         )}
@@ -47,12 +44,11 @@ const shirt = () => {
           <Decal
             position={[-0.0002, -0.0013, 0.16]}
             rotation={[0, 0, 0]}
-            scale={0.30}
+            scale={0.3}
             map={logoTexture}
             //map-anisotropy={16}
             depthTest={false}
             depthWrite={true}
-
           />
         )}
       </mesh>
